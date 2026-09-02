3dc2323738e2bd4591f6961d0cd831114e60e4c8
yamusi001 <hafsatabubakarsulaiman543@gmail.com>
2026-09-02 19:08:40 +0100

    Feature: Add comprehensive Ayat As-Sajdah 15 prostration verses index

---
+++ src/data/sajdahAyahs.ts
@@ -0,0 +1,25 @@
+export interface SajdahAyahItem {
+  surahNumber: number;
+  surahName: string;
+  ayahNumber: number;
+  juzNumber: number;
+  hizbNumber: number;
+}
+
+export const SAJDAH_AYAHS: SajdahAyahItem[] = [
+  { surahNumber: 7, surahName: "Al-A'raf", ayahNumber: 206, juzNumber: 9, hizbNumber: 18 },
+  { surahNumber: 13, surahName: "Ar-Ra'd", ayahNumber: 15, juzNumber: 13, hizbNumber: 26 },
+  { surahNumber: 16, surahName: "An-Nahl", ayahNumber: 50, juzNumber: 14, hizbNumber: 27 },
+  { surahNumber: 17, surahName: "Al-Isra", ayahNumber: 109, juzNumber: 15, hizbNumber: 30 },
+  { surahNumber: 19, surahName: "Maryam", ayahNumber: 58, juzNumber: 16, hizbNumber: 31 },
+  { surahNumber: 22, surahName: "Al-Hajj", ayahNumber: 18, juzNumber: 17, hizbNumber: 33 },
+  { surahNumber: 22, surahName: "Al-Hajj", ayahNumber: 77, juzNumber: 17, hizbNumber: 34 },
+  { surahNumber: 25, surahName: "Al-Furqan", ayahNumber: 60, juzNumber: 19, hizbNumber: 37 },
+  { surahNumber: 27, surahName: "An-Naml", ayahNumber: 26, juzNumber: 19, hizbNumber: 38 },
+  { surahNumber: 32, surahName: "As-Sajdah", ayahNumber: 15, juzNumber: 21, hizbNumber: 42 },
+  { surahNumber: 38, surahName: "Sad", ayahNumber: 24, juzNumber: 23, hizbNumber: 45 },
+  { surahNumber: 41, surahName: "Fussilat", ayahNumber: 38, juzNumber: 24, hizbNumber: 48 },
+  { surahNumber: 53, surahName: "An-Najm", ayahNumber: 62, juzNumber: 27, hizbNumber: 54 },
+  { surahNumber: 84, surahName: "Al-Inshiqaq", ayahNumber: 21, juzNumber: 30, hizbNumber: 59 },
+  { surahNumber: 96, surahName: "Al-Alaq", ayahNumber: 19, juzNumber: 30, hizbNumber: 60 },
+];
